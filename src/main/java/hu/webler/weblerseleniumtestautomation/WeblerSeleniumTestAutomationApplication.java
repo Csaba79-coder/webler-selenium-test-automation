e9e0d1f1e3ee2e91abd21e445cb76c0519a7a1d7
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-13 10:18:26 +0200

    Initial commit

---
+++ src/main/java/hu/webler/weblerseleniumtestautomation/WeblerSeleniumTestAutomationApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerseleniumtestautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerSeleniumTestAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerSeleniumTestAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/webler/weblerseleniumtestautomation/WeblerSeleniumTestAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerseleniumtestautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerSeleniumTestAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
